3b5fcd1732c14b646291733d073663ea1d787e67
RFcobo <rafaelcobo@outlook.com>
2024-11-25 15:25:51 -0300

    Pequenas modificações - Realloc funcionando

--- Novo_cliente.c
+++ Novo_cliente.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "ListaOrdenada.h"
-#include "ListaOrdenada.c"
+#include "aloc_dinamica/ListaOrdenada.h"
+#include "aloc_dinamica/ListaOrdenada.c"
 
 int main(){
 
@@ -17,22 +17,20 @@ int main(){
     
 
         for (int i = 0; i < tamListUser; i++) {
-           /* if (i>= lista.alocacao){
-                realocLista(&lista);
-            }*/
+
             printf("Digite o valor: ");
             scanf("%d", &reg.chave);
             inserirElemListaOrd(&lista, reg);
-            //lista.nroElem+=1;
+
         }
 
-
     exibirLista(&lista);
+
     printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
     printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
- //   printf("Tamanho da lista (em bytes): %i.\n", ((sizeof(LISTA) -8)- (tamListUser*4))); //Calculando o tamanho restante da lista supondo o alinhamento de 8bytes para o processador.
-
+    //printf("Tamanho da lista (em bytes): %i.\n", ((sizeof(LISTA) -8)- (tamListUser*4))); //Calculando o tamanho restante da lista supondo o alinhamento de 8bytes para o processador.
 
     free(lista.A);
+
     return 0;
 }
