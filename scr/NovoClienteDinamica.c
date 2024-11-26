9910c36c61feca4f2b3969496f9227f12d31163c
RFCobo <rafaelcobo@outlook.com>
2024-11-26 19:22:53 -0300

    final

--- scr/NovoClienteDinamica.c
+++ scr/NovoClienteDinamica.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../include/ListaOrdenada.h"
-#include "../include/ListaOrdenada.c"
+#include "../include/ListaOrdenadaDinamica.h"
+#include "ListaOrdenadaDinamica.c"
 
 
 
