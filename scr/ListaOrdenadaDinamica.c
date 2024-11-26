d1a0f21586d917c5396c71d05866419d20b737ec
RF Cobo <107076651+rfcob@users.noreply.github.com>
2024-11-26 17:19:01 -0300

    Add files via upload

---
+++ scr/ListaOrdenadaDinamica.c
@@ -0,0 +1,266 @@
+// ListaOrdenada.c
+#include "ListaOrdenada.h"
+
+
+// Inicialização da lista sequencial dinâmica (apontada pelo endereço em l)||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+  void inicializarLista(LISTA* l){
+      l->A = (REGISTRO*)malloc(MAX * sizeof(REGISTRO));
+
+      if (l->A == NULL) {
+      printf("Erro ao alocar memória\n");
+      exit(1);
+      }
+
+      l->nroElem = 0;
+      l->alocacao = MAX; 
+  }
+
+//função para preencher/popular a lista|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+  void preencher_lista(LISTA* l, REGISTRO reg){
+
+      int tamListUser;
+
+      printf("Digite a quantidade de itens a inserir:\n");
+      scanf("%d", &tamListUser);
+      
+
+          for (int i = 0; i < tamListUser; i++)  {
+
+              printf("Digite o valor: ");
+              scanf("%d", &reg.chave);
+              inserirElemListaOrd(l, reg);
+              //inserirElemListaOrdSemDup(lista, reg); 
+
+          }
+
+  }
+
+//Realocar espaço caso necessário||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+  void realocLista(LISTA *l) {
+
+    l->alocacao *= 2;
+
+    l->A = (REGISTRO*) realloc(l->A, l->alocacao * sizeof(REGISTRO));
+
+    if (l->A == NULL){
+      printf("Erro ao alocar memória\n");
+      exit(1);
+    }
+
+}
+
+//Desalocar espaço caso necessário|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+  void desalocLista(LISTA *l) {
+    
+    l->alocacao/2;
+
+    l->A = (REGISTRO*) realloc(l->A, l->alocacao * sizeof(REGISTRO));
+
+    if (l->A == NULL){
+      printf("Erro ao alocar memória\n");
+      exit(1);
+  }
+
+}
+
+// Exibir/Imprimir|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+  void exibirLista(LISTA* l){
+      printf("Lista: \" ");
+
+      for (int i=0; i < l->nroElem; i++){
+        printf("%i ", l->A[i].chave);
+      }
+      printf("\"\n");
+  } 
+
+// Retornar o tamanho da lista (numero de elementos "validos")||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+  int tamanho(LISTA* l){
+    return l->nroElem;
+  } 
+
+
+//* Retornar o tamanho da lista em bytes|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+  int tamanhoEmBytes(LISTA* l) {
+    return l->nroElem * sizeof(REGISTRO);
+  } 
+
+//Inserir elementos||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+  bool inserirElemListaOrd(LISTA* l, REGISTRO reg) {
+      // Verificar se é necessário realocar a lista
+      if (l->nroElem >= l->alocacao) {
+          realocLista(l); // Realoca a lista se atingir a capacidade
+      }
+
+      int pos = l->nroElem;
+      while (pos > 0 && l->A[pos - 1].chave > reg.chave) {
+          l->A[pos] = l->A[pos - 1];
+          pos--;
+      }
+      l->A[pos] = reg;
+      l->nroElem++;
+      return true;
+  } 
+
+//Exelcuir elemento na lista ordenada dinamicamente||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+  bool excluirElemListaOrd(LISTA* l, TIPOCHAVE ch){ 
+      int pos, j;
+      int teste;
+      teste=l->alocacao/2;
+      pos = buscaBinaria(l,ch);
+      
+
+      if(pos == ERRO){
+        return false;
+      } 
+
+      if (l->nroElem >= teste) {
+          desalocLista(l); 
+
+      for(j = pos; j < l->nroElem-1; j++){
+          l->A[j] = l->A[j+1];
+      }
+
+      l->nroElem--;
+
+      return true;
+    } 
+
+  }
+
+//Retornar o primeiro elemento sequencial da lista ou erro se a lista estiver vazia||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+TIPOCHAVE primeiroElem(LISTA* l){
+  if(l->nroElem > 0){
+    return l->A[0].chave;
+  }else{
+    return ERRO;
+    }
+} 
+
+//Retornar o último elemento sequencial da lista ou erro se a lista estiver vazia|||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+TIPOCHAVE ultimoElem(LISTA* l) {
+  if(l->nroElem > 0){
+    return l->A[l->nroElem-1].chave;
+  }else{
+    return ERRO;
+    }
+}
+
+// Busca sequencial em lista ordenada ou não SEM SENTINELA |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+int buscaSequencial(LISTA* l, TIPOCHAVE ch) {
+
+  int i = 0;
+  while (i < l->nroElem){
+    if(ch == l->A[i].chave){
+      return i; //encontrou o numero
+    }else{
+      i++;
+    }
+  }
+  return ERRO; // não achou
+}
+
+// Retornar a chave do elemento que está na posição n da LISTA. Lembre-se que as posicoes do arranjo A vao de 0 a MAX-1|||||||||||||||
+
+TIPOCHAVE enesimoElem(LISTA* l, int n) {
+  if( (n >= 0) && (n < l->nroElem)){
+     return l->A[n].chave;
+  }else{
+  return ERRO;
+  } 
+}
+
+
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+//NÃO TESTADAS AINDA 
+
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+
+/* Reinicializar a estrutura */
+void reinicializarLista(LISTA* l) {
+  l->nroElem = 0;
+} /* reinicializarLista */
+
+
+/* Busca sequencial em lista COM SENTINELA (vetor criado com MAX+1 posições) */
+int buscaSentinela(LISTA* l, TIPOCHAVE ch) {
+  int i = 0;
+  l->A[l->nroElem].chave = ch; // sentinela
+  while(l->A[i].chave != ch) i++;
+  if (i > l->nroElem -1) return ERRO; // não achou
+  else return i;
+} /* buscaSentinela */
+
+/* Busca sequencial em lista COM SENTINELA (vetor criado com MAX+1 posições) 
+   considerando o arranjo ordenado */
+int buscaSentinelaOrdenada(LISTA* l, TIPOCHAVE ch) {
+  int i = 0;
+  l->A[l->nroElem].chave = ch; // sentinela
+  while(l->A[i].chave < ch) i++;
+  if (i == l->nroElem || l->A[i].chave != ch) return ERRO; // não achou
+  else return i;
+} /* buscaSentinela */
+
+/* Busca binaria em lista ordenada */
+int buscaBinaria(LISTA* l, TIPOCHAVE ch){
+  int esq, dir, meio;
+  esq = 0;
+  dir = l->nroElem-1;
+  while(esq <= dir) {
+    meio = ((esq + dir) / 2);
+    if(l->A[meio].chave == ch) return meio; // achou
+    else {
+      if(l->A[meio].chave < ch) esq = meio + 1;
+      else dir = meio - 1;
+    }
+  }
+  return ERRO;
+} /* buscaBinaria */
+
+
+/* Exclusão do elemento cuja chave seja igual a ch */
+bool excluirElemLista(LISTA* l, TIPOCHAVE ch) { 
+  int pos, j;
+  pos = buscaSequencial(l,ch);
+  if(pos == ERRO) return false; // não existe
+  for(j = pos; j < l->nroElem-1; j++) l->A[j] = l->A[j+1];
+  l->nroElem--;
+  return true;
+} /* excluirElemLista */
+
+
+/* Inserção em lista ordenada usando busca binária sem duplicação */
+bool inserirElemListaOrdSemDup(LISTA* l, REGISTRO reg) {
+  if(l->nroElem >= MAX) return false; // lista cheia
+  int pos;
+  pos = buscaBinaria(l,reg.chave);
+  if(pos != ERRO) return false; // elemento já existe
+  pos = l->nroElem-1;
+  while(pos>0 && l->A[pos].chave > reg.chave) {
+    l->A[pos+1] = l->A[pos];
+    pos--;
+  }
+  l->A[pos+1] = reg;
+  l->nroElem++;
+  return true;
+} /* inserirElemListaOrd */
+
+
---
+++ scr/NovoClienteDinamica.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "aloc_dinamica/ListaOrdenada.h"
+#include "aloc_dinamica/ListaOrdenada.c"
+
+
+
+int main(){
+
+    int valorExcluir;
+
+    LISTA lista;
+    REGISTRO reg;
+    
+    inicializarLista(&lista); //-------------------------------------iniciar a lista com alocação dinâmica
+
+    preencher_lista(&lista, reg); //---------------------------------------------------------------popular
+
+    exibirLista(&lista);//--------------------------------------------------------------------primeira vez
+
+    printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
+    printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
+
+    while(true){//-----------------------------------para testar inlcusão e exclusão de maneira "dinamica"
+        int escolha;
+        printf("Digite 1 par exluir elementos ou 0 para sair:\n");
+        scanf("%d", &escolha);
+       if(escolha==1){
+            int quantidade;
+            printf("Digite quantos elementos deseja excluir: \n"); 
+            
+            scanf("%d", &quantidade);
+
+            for(int k = 0; k<quantidade; k++){
+                scanf("%d", &valorExcluir);
+                excluirElemListaOrd(&lista, valorExcluir);
+            }
+        }else{
+            break;
+        }
+    }
+
+    exibirLista(&lista);//---------------------------------------------------------------------segunda vez
+
+    printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
+    printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
+
+    printf("Primeiro elemento da lista: %i.\n", primeiroElem(&lista));
+    printf("Chave 6 do elemento lista - retorna: %i.\n", enesimoElem(&lista, 6));
+    printf("O numero 6 está na posicão da lista: %i.\n", buscaSequencial(&lista, 6));
+    //printf("Tamanho da lista (em bytes): %i.\n", ((sizeof(LISTA) -8)- (tamListUser*4))); //Calculando o tamanho restante da lista supondo o alinhamento de 8bytes para o processador.
+
+    free(lista.A);
+
+    return 0;
+}
